2472b0bd677a558c346063dce302d4d8b4a6bc7f
NguyenQuocTuan385 <nguyenquoctuan385@gmail.com>
2023-10-24 20:17:26 +0700

    sakila-hw3

--- src/main/java/com/group06/sakila/controller/ActorController.java
+++ src/main/java/com/group06/sakila/controller/ActorController.java
@@ -1,12 +1,8 @@
 package com.group06.sakila.controller;
 
 import com.group06.sakila.entity.Actor;
-import com.group06.sakila.exception.ErrorResponse;
 import com.group06.sakila.requestmodel.ActorRequest;
 import com.group06.sakila.service.ActorService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,50 +13,34 @@ import java.util.List;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/actors")
-@Tag(name = "Actors API")
 public class ActorController {
     private ActorService actorService;
 
     // Get all actors
-    @Operation(summary = "Get all actors")
-    @ApiResponse(responseCode = "200", description = "List of actors")
     @GetMapping("")
     ResponseEntity<List<Actor>> getAllActors() {
         return actorService.findAll();
     }
 
     //Get detail actor
-    @Operation(summary = "Get actor by ID")
-    @ApiResponse(responseCode = "200", description = "Found the actor")
-    @ApiResponse(responseCode = "404", description = "Actor not found")
     @GetMapping("/{id}")
     ResponseEntity<Actor> findById(@PathVariable Long id) {
         return actorService.findById(id);
     }
 
     //Create actor
-    @Operation(summary = "Create a new actor")
-    @ApiResponse(responseCode = "201", description = "Actor is created")
-    @ApiResponse(responseCode = "400", description = "Invalid input")
     @PostMapping("")
     ResponseEntity<Actor> createActor(@RequestBody @Valid ActorRequest actorRequest) {
         return actorService.createActor(actorRequest);
     }
 
     //Update actor
-    @Operation(summary = "Update actor by ID")
-    @ApiResponse(responseCode = "200", description = "Updated the actor")
-    @ApiResponse(responseCode = "400", description = "Invalid input")
-    @ApiResponse(responseCode = "404", description = "actor not found")
     @PutMapping("/{id}")
     ResponseEntity<Actor> updateActor(@RequestBody @Valid ActorRequest actorRequest, @PathVariable Long id) {
         return actorService.updateActor(actorRequest, id);
     }
 
     // Delete actor
-    @Operation(summary = "Delete actor by ID")
-    @ApiResponse(responseCode = "204", description = "Actor is deleted")
-    @ApiResponse(responseCode = "404", description = "Actor not found")
     @DeleteMapping("/{id}")
     ResponseEntity<String > deleteActor(@PathVariable Long id) {
         return actorService.deleteById(id);
--- src/main/java/com/group06/sakila/controller/FilmController.java
+++ src/main/java/com/group06/sakila/controller/FilmController.java
@@ -3,14 +3,6 @@ package com.group06.sakila.controller;
 import com.group06.sakila.requestmodel.FilmRequest;
 import com.group06.sakila.entity.Film;
 import com.group06.sakila.service.FilmService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,171 +13,31 @@ import java.util.List;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/films")
-@Tag(name = "Films API")
 public class FilmController {
     private FilmService filmService;
 
-    @Operation(
-            summary = "Get all films",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "List of films",
-                            content = @Content(
-                                    mediaType = "application/json",
-                                    array = @ArraySchema(schema = @Schema(implementation = Film.class))
-                                    )
-                    )
-            }
-    )
     @GetMapping("")
     ResponseEntity<List<Film>> getAllFilms() {
         return filmService.findAll();
     }
 
-    @Operation(
-            summary = "Get film by ID",
-            parameters = {@Parameter(name = "id", description = "The film id", example = "3")},
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "Found the film",
-                            content = @Content(
-                                    mediaType = "application/json",
-                                    examples = @ExampleObject(
-                                            value = """
-                                                    {
-                                                      "id": 3,
-                                                      "title": "SLIPPER FIDELITY",
-                                                      "description": "A Taut Reflection of a Secret Agent And a Man who must Redeem a Explorer in A MySQL Convention",
-                                                      "releaseYear": 2023,
-                                                      "languageId": 1,
-                                                      "originalLanguageId": 1,
-                                                      "rentalDuration": 4,
-                                                      "rentalRate": 2.99,
-                                                      "length": 132,
-                                                      "replacementCost": 27.99,
-                                                      "rating": "PG-13",
-                                                      "specialFeatures": "Commentaries,Deleted Scenes",
-                                                      "lastUpdate": "2006-02-15 05:03:42"                                            
-                                                    }
-                                                    """
-                                    )
-                            )
-
-                    ),
-                    @ApiResponse(responseCode = "404", ref = "resourceNotFoundAPI"),
-            }
-    )
     @GetMapping("/{id}")
     ResponseEntity<Film> findById(@PathVariable Integer id) {
         return filmService.findById(id);
     }
 
     //Create film
-    @Operation(
-            summary = "Create a new film",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "Created the film",
-                            content = @Content(
-                                    schema = @Schema(implementation = Film.class),
-                                    mediaType = "application/json"
-                            )
-
-                    ),
-                    @ApiResponse(responseCode = "400", ref = "badRequestAPI"),
-    })
     @PostMapping("")
-    ResponseEntity<Film> createFilm(@io.swagger.v3.oas.annotations.parameters.RequestBody(
-            content = @Content(
-                    mediaType = "application/json",
-                    examples = {
-                            @ExampleObject(
-                                    value = """
-                                        {
-                                          "title": "SLIPPER FIDELITY",
-                                          "description": "A Taut Reflection of a Secret Agent And a Man who must Redeem a Explorer in A MySQL Convention",
-                                          "releaseYear": 2023,
-                                          "languageId": 1,
-                                          "originalLanguageId": 1,
-                                          "rentalDuration": 4,
-                                          "rentalRate": 2.99,
-                                          "length": 132,
-                                          "replacementCost": 27.99,
-                                          "rating": "PG-13",
-                                          "specialFeatures": "Commentaries,Deleted Scenes"
-                                        }
-                                        """
-                            )
-                    }
-            )
-    ) @RequestBody @Valid FilmRequest filmRequest) {
+    ResponseEntity<Film> createFilm(@RequestBody @Valid FilmRequest filmRequest) {
         return filmService.createFilm(filmRequest);
     }
 
-    @Operation(
-            summary = "Update film by ID",
-            parameters = {@Parameter(name = "id", description = "The film id", example = "3")},
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "Updated the film",
-                            content = @Content(
-                                    schema = @Schema(implementation = Film.class),
-                                    mediaType = "application/json"
-                            )
-
-                    ),
-                    @ApiResponse(responseCode = "404", ref = "resourceNotFoundAPI"),
-                    @ApiResponse(responseCode = "400", ref = "badRequestAPI"),
-            })
     @PutMapping("/{id}")
-    ResponseEntity<Film> updateActor(@io.swagger.v3.oas.annotations.parameters.RequestBody(
-            content = @Content(
-                    mediaType = "application/json",
-                    examples = {
-                            @ExampleObject(
-                                    value = """
-                                            {
-                                              "title": "SLIPPER FIDELITY",
-                                              "description": "A Taut Reflection of a Secret Agent And a Man who must Redeem a Explorer in A MySQL Convention",
-                                              "releaseYear": 2023,
-                                              "languageId": 1,
-                                              "originalLanguageId": 1,
-                                              "rentalDuration": 4,
-                                              "rentalRate": 2.99,
-                                              "length": 132,
-                                              "replacementCost": 27.99,
-                                              "rating": "PG-13",
-                                              "specialFeatures": "Commentaries,Deleted Scenes"
-                                            }
-                                            """
-                            )
-                    }
-            )
-    ) @RequestBody @Valid FilmRequest filmRequest, @PathVariable Integer id) {
+    ResponseEntity<Film> updateActor(@RequestBody @Valid FilmRequest filmRequest, @PathVariable Integer id) {
         return filmService.updateFilm(filmRequest, id);
     }
 
     // Delete film
-    @Operation(
-            summary = "Delete film by ID",
-            parameters = {@Parameter(name = "id", description = "The film id", example = "3")},
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "Deleted the film",
-                            content = @Content(
-                                    examples = @ExampleObject(
-                                            value = "Deleted film successfully"
-                                    )
-                            )
-
-                    ),
-                    @ApiResponse(responseCode = "404", ref = "resourceNotFoundAPI"),
-            })
     @DeleteMapping("/{id}")
     ResponseEntity<String> deleteFilm(@PathVariable Integer id) {
         return filmService.deleteById(id);
